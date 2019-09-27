e14cc955408a986b42400c561f2183957094a8f1
Alan Dodge <alphaechododge@gmail.com>
2019-09-27 01:31:59 +0000

    Implemented file client and server using net, only supports txt for now

---
+++ file-client.js
@@ -0,0 +1,48 @@
+const net = require('net');
+const readline = require('readline');
+const fs = require('fs');
+
+const conn = net.createConnection({ 
+  host: 'localhost', // change to IP address
+  port: 4000
+});
+
+conn.on('connect', () => {
+  conn.setEncoding('utf8');
+  let fileName = "";
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  rl.on('line', (input) => {
+    conn.write(input);
+    fileName = input;
+  });
+
+  conn.on('data', (data) => {
+    if (data === "404") {
+      console.log("No such file");
+      return;
+    }
+    let writeStream = fs.createWriteStream(fileName);
+    writeStream.write(data);
+    
+    writeStream.on('error', (err) => {
+      console.log('error:', err.message);
+    });
+  
+    writeStream.on('finish', (err) => {
+      if (err) {
+        console.log("error:", err)
+      } else {
+        console.log(`Downloaded and saved ${writeStream.bytesWritten} to ${fileName}`);
+      }
+    });
+  
+    writeStream.end();
+  });
+
+});
+
---
+++ file-server.js
@@ -0,0 +1,41 @@
+const net = require('net');
+const readline = require('readline');
+const fs = require('fs');
+
+
+const server = net.createServer(function() {
+});
+
+server.listen(4000, () => {
+  console.log('Server listening on port 4000!');
+});
+
+server.on('connection', (client) => {
+  console.log('New client connected!');
+  // client.write('Request a file.');
+
+  client.setEncoding('utf8'); // interpret data as text
+  client.on('data', data => {
+    console.log('Message from client: ', data);
+    let filePath = './data/' + data;
+
+    let readStream = fs.createReadStream(filePath);
+    
+    readStream.on('error', (err) => {
+      console.log('error:', err.message);
+      client.write("404");
+    });
+
+    readStream.pipe(client);
+    
+  });
+
+  // const rl = readline.createInterface({
+  //   input: process.stdin,
+  //   output: process.stdout
+  // });
+  
+  // rl.on('line', (input) => {
+  //   client.write(input);
+  // });
+});
